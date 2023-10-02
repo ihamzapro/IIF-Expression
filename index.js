10f9be543f179a43e8659816767093bb1546ea5d
Hamza Khan <hamza.khan951hk@gmail.com>
2023-10-02 22:00:56 +0500

    IIFE

---
+++ index.js
@@ -0,0 +1,5 @@
+// It is a design pattern which is also called Self-Executing Anonymous function and contain two major parts//
+
+(function (aName) {
+  console.log(`Author name is ${aName}?`);
+})("Hamza Khan");
